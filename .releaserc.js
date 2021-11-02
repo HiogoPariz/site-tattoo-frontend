b104a5483cf447393fcdc6394f63ef2b0333a8e4
HiogoPariz <hiogopariz@gmail.com>
2021-11-02 01:43:02 -0300

    ci: release plugins to changelog generator

--- .releaserc.js
+++ .releaserc.js
@@ -9,8 +9,8 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/git',
     '@semantic-release/changelog',
+    '@semantic-release/git',
     '@semantic-release/github'
   ]
 }
