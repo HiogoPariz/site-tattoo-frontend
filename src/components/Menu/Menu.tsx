30b69ee9f29bd15985dae577da7426c2f504645f
HiogoPariz <hiogopariz@gmail.com>
2021-11-02 19:15:56 -0300

    feat: layout com footer e header

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
-export const Menu: React.FC = ({ children }) => {
-  return <div>{children}</div>
+export const Menu: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/teste">Teste</Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
