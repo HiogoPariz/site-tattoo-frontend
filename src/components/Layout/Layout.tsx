88ef93baf56776f26967b8610070523ca7fa80c1
Hiogo de Pariz Cavalheiro <hiogopariz@gmail.com>
2021-11-02 19:49:33 -0300

    Staging (#10)
    * Feat dockering (#4)
    
    * feat: docker
    
    * test: jest config
    
    * fix: layout duplicate
    
    * style: main container
    
    * fix: lint
    
    * fix: jest
    
    * test: updated tests
    
    * feat: layout com footer e header (#9)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { MainContainer } from '@/styles/containers/main'
+import { Menu } from '@/components/Menu/Menu'
+import { Footer } from '@/components/Footer/Footer'
 
 export const Layout: React.FC = ({ children }) => {
-  return <MainContainer>{children}</MainContainer>
+  return (
+    <MainContainer>
+      <Menu />
+
+      {children}
+      <Footer />
+    </MainContainer>
+  )
 }
