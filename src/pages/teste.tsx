dd50dd10d64d3a3712bd5a964c15fd0a41ba9dab
Hiogo de Pariz Cavalheiro <hiogopariz@gmail.com>
2021-11-02 19:19:47 -0300

    feat: layout com footer e header (#9)

---
+++ src/pages/teste.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Teste: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Site tattoo</title>
+        <meta name="description" content="Site tattoo" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <h1>Teste</h1>
+      <p>Teste site tattoo</p>
+    </div>
+  )
+}
+
+export default Teste
